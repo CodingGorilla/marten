ad0ffce6baa4642643ff97065aece7565b1cdc27
Mark Warpool <codinggorilla@outlook.com>
2016-05-18 08:49:13 -0400

    Adding some code comments to help explain what's going on

--- src/DinnerParty/Models/Marten/LastUpdatedSessionListener.cs
+++ src/DinnerParty/Models/Marten/LastUpdatedSessionListener.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using DinnerParty.Helpers;
 using Marten;
-using Microsoft.Ajax.Utilities;
 
 namespace DinnerParty.Models.Marten
 {
-    // This class ensures that the LastModified property/field is always updated before changes
-    // are persisted by marten to PostgreSQL
+    /// <summary>
+    /// This class demonstrates using a <seealso cref="DocumentSessionListenerBase"/> to ensure that 
+    /// the <see cref="Dinner.LastModified"/> property is always updated before an insert/update opration
+    /// </summary>
+    /// <remarks>
+    /// See: https://github.com/JasperFx/marten/blob/master/documentation/documentation/documents/diagnostics.md#listening-for-document-store-events
+    /// </remarks>
     public class LastUpdatedSessionListener : DocumentSessionListenerBase
     {
         public override void BeforeSaveChanges(IDocumentSession session)
         {
+            // Get a set of pending changes for this session
             var pending = session.PendingChanges;
 
+            // For each dinner that is to be inserted, set the Dinner.LastModified property to now
             pending.InsertsFor<Dinner>().Each(d => d.LastModified = DateTime.Now);
+
+            // For each dinner that is to be updated, set the Dinner.LastModified property to now
             pending.UpdatesFor<Dinner>().Each(d => d.LastModified = DateTime.Now);
         }
     }
--- src/DinnerParty/Models/Marten/UserModelRegistry.cs
+++ src/DinnerParty/Models/Marten/UserModelRegistry.cs
@@ -3,12 +3,20 @@ using Marten;
 
 namespace DinnerParty.Models.Marten
 {
+    /// <summary>
+    /// A <seealso cref="MartenRegistry"/> to configure document storage for the <see cref="UserModel"/> model
+    /// </summary>
+    /// <remarks>
+    /// This class demonstrates adding searchable fields to the document's table
+    /// see: https://github.com/JasperFx/marten/blob/master/documentation/documentation/documents/customizing.md#searchable-fields
+    /// </remarks>
     public class UserModelRegistry : MartenRegistry
     {
         public UserModelRegistry()
         {
             // Generate a searchable index for UserModel.Username
             For<UserModel>().Searchable(u => u.Username);
+            
             // Generate a searchable index for UserModel.EMailAddress
             For<UserModel>().Searchable(u => u.EMailAddress);
         }
